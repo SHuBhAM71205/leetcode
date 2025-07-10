15e2c6d2c326bbcc111f6077d53739c1d69260c0
SHuBhAM71205 <sp710812gs@gmail.com>
2025-07-10 08:07:31 +0530

    Add implementation of romanToInt function and update README for Day 3

---
+++ Day3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> maping = {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        char previous=' ';
+        int ans=0;
+        for(auto roman_num : s)
+        {
+            if(previous != ' ' && maping[previous]<maping[roman_num])
+            {
+
+                ans+=(maping[roman_num]-maping[previous]);
+            }
+            else{
+                ans+=maping[roman_num];
+            }
+            previous=roman_num;
+        }
+
+        return ans;
+    }
+};
